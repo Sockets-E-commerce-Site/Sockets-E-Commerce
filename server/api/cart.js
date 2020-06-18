a2598af0bef246d2dcf0ca202beec3cd6e582a7b
Thaddeus Metz <temetz90@gmail.com>
2020-06-18 13:50:28 -0500

    added route to alter quantity of product in cart

--- server/api/cart.js
+++ server/api/cart.js
@@ -1,3 +1,46 @@
 const router = require('express').Router()
 const {Product, User, ProductOrder, Order} = require('../db/models')
+
 module.exports = router
+
+router.put('/edit', async (req, res, next) => {
+  try {
+    const {userId, productId, productQuantity} = req.body
+    const cart = await Order.findOne({
+      where: {
+        userId,
+        status: 'in cart'
+      },
+      include: Product
+    })
+
+    const foundProduct = cart.products.find(product => {
+      return product.id === parseInt(productId)
+    })
+
+    if (
+      productQuantity <= foundProduct.invQuantity &&
+      productQuantity > 0 &&
+      foundProduct.invQuantity > 0
+    ) {
+      await foundProduct.productOrder.update(
+        {productQuantity: productQuantity},
+        {
+          returning: true
+        }
+      )
+    }
+
+    const updatedCart = await Order.findOne({
+      where: {
+        userId,
+        status: 'in cart'
+      },
+      include: Product
+    })
+
+    res.json(updatedCart)
+  } catch (error) {
+    next(error)
+  }
+})
