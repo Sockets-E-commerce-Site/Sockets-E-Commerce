db0cee256484a4036265055798af9faa817af765
Thaddeus Metz <temetz90@gmail.com>
2020-06-22 10:47:03 -0500

    back to home button on error page up and working

--- client/components/Utility/ErrorPage.js
+++ client/components/Utility/ErrorPage.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import {NavLink} from 'react-router-dom'
 
 export default function ErrorPage() {
   return (
     <div>
       <h1>Whoops! Something went wrong!</h1>
+      <button
+        type="button"
+        className="bg-gray-500 w-30 hover:bg-blue-700 text-white px-2  rounded"
+      >
+        <NavLink to="/home">Go Back Home</NavLink>
+      </button>
     </div>
   )
 }
